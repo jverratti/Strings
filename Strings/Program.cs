c5937d7ea8650790e7a528385d9f23a1bc2317d3
jverratti <31357738+jverratti@users.noreply.github.com>
2017-09-20 19:53:15 -0600

    Strings

---
+++ Strings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Strings
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            StringBuilder myString = new StringBuilder();
+
+            for (int i -0; i < 100, i++)
+            {
+                myString.Append("--");
+                myString.Append(i);
+
+            }
+*/
+            string myString = "Lyrics";
+
+                myString = myString.Substring(5, 14);
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
+
+
+//
